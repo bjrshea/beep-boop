7e10715440248f99c7d5ab4c0b779c67449e453a
Brendan Shea <padma@email.com>
2018-08-17 09:36:07 -0700

    Begin if/else statements in backend logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,16 @@
 var userInput = function(input) {
-  return "beep!"
-}
+  if (input % 3 === 0) {
+    return "I'm sorry, Dave. I'm afraid I can't do that.";
+  } else {
+    return "beep!";
+  }
+};
 
 // User Interface Logic:
 $(document).ready(function() {
   $("#beep-boop").submit(function(event) {
     event.preventDefault();
-    var input = parseInt($("user-input").val());
+    var input = parseInt($("#user-input").val());
     var result = userInput(input);
     $("#result").text(result);
   });
