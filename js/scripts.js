49c61cb9f2f62d821ff2cbe9ffffee9751cbaaa9
Brendan Shea <padma@email.com>
2018-08-17 13:25:50 -0700

    Finish if/else statements which completes JS logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,10 @@
 // Business Logic:
 var userInput = function(input) {
-
   var inputToString = input.toString();
   var inputRange = [];
   for (var startingNumber = 0; startingNumber <= input; startingNumber += 1) {
     inputRange.push(startingNumber);
   }
-
   if (input % 3 === 0) {
     return "I'm sorry, Dave. I'm afraid I can't do that.";
   } else if (inputToString.includes("1")) {
@@ -17,7 +15,6 @@ var userInput = function(input) {
     return inputRange;
   }
 };
-
 // User Interface Logic:
 $(document).ready(function() {
   $("#beep-boop").submit(function(event) {
