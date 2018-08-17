e03557795457f46e392170e7357495f55c0a5de0
Brendan Shea <padma@email.com>
2018-08-17 11:45:34 -0700

    Add for loop to business logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,18 @@
+// Business Logic:
+
 var userInput = function(input) {
-  if (input % 3 === 0) {
-    return "I'm sorry, Dave. I'm afraid I can't do that.";
-  } else {
-    return "beep!";
+
+  var userResult = [];
+  for (var index = 0; index <= input; index += 1) {
+    if (input % 3 === 0) {
+      return "I'm sorry, Dave. I'm afraid I can't do that.";
+    } else if (input === 1) {
+      return "boop!";
+    } else if (input === 0) {
+      return "beep!";
+    } else {
+      alert(index);
+    }
   }
 };
 
